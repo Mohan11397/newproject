b6a785e38944102318c5ecd66bf05358dafcfc31
Mohan11397 <mohan11397@gmail.com>
2021-05-05 16:14:15 +0530

    new project

---
+++ src/org/selenium/SeleniumDemo.java
@@ -0,0 +1,33 @@
+package org.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumDemo {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "C:\\eclipse\\SeleniumFirstDay\\WebDriver\\chromedriver.exe");
+	
+	WebDriver driver= new ChromeDriver();
+	
+	// to launch the url
+	
+	driver.get("https://www.facebook.com/");
+	
+	//to get title
+	
+	 String title = driver.getTitle();
+
+System.out.println(title);
+	
+	//to get the current url
+	
+	String currentUrl = driver.getCurrentUrl();
+	System.out.println(currentUrl);
+	
+	// to close the browser
+	
+	driver.quit();
+	System.out.println("webpage closed");
+	
+}
+}
